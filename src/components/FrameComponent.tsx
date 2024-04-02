2dd3251f99d235e76be31760686d6d03abd04d2c
thanh-locofy <thanh-locofy>
2024-04-02 14:24:15 +0000

    Pushed from Locofy Builder

--- src/components/FrameComponent.tsx
+++ src/components/FrameComponent.tsx
@@ -8,32 +8,41 @@ const FrameComponent: FunctionComponent = () => {
         <h1 className={styles.createAccount1}>Create Account</h1>
         <div className={styles.field}>
           <div className={styles.field1}>
-            <div className={styles.inputField}>
-              <input className={styles.email} placeholder="Email" type="text" />
+            <div className={styles.loopController}>
+              <div className={styles.email}>Email</div>
+              <div className={styles.inputField}>
+                <div className={styles.thanhlocofyai}>thanh@locofy.ai</div>
+              </div>
             </div>
-            <div className={styles.inputField1}>
-              <input
-                className={styles.password}
-                placeholder="Password"
-                type="text"
-              />
-              <img
-                className={styles.eyeOpen1Icon}
-                alt=""
-                src="/eyeopen-1.svg"
-              />
+            <div className={styles.loopController1}>
+              <div className={styles.password}>Password</div>
+              <div className={styles.inputField1}>
+                <input
+                  className={styles.input}
+                  placeholder="*****************"
+                  type="text"
+                />
+                <img
+                  className={styles.eyeOpen1Icon}
+                  alt=""
+                  src="/eyeopen-1.svg"
+                />
+              </div>
             </div>
-            <div className={styles.inputField2}>
-              <input
-                className={styles.confirmPassword}
-                placeholder="Confirm Password"
-                type="text"
-              />
-              <img
-                className={styles.eyeOpen1Icon1}
-                alt=""
-                src="/eyeopen-1.svg"
-              />
+            <div className={styles.loopController2}>
+              <div className={styles.retypePassword}>Retype password</div>
+              <div className={styles.inputField2}>
+                <input
+                  className={styles.input1}
+                  placeholder="*****************"
+                  type="text"
+                />
+                <img
+                  className={styles.eyeOpen1Icon1}
+                  alt=""
+                  src="/eyeopen-1.svg"
+                />
+              </div>
             </div>
           </div>
           <div className={styles.remember}>
